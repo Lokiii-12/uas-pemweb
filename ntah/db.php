ce609d106eb45e86b04c409ac4141bd4323dc0ad
graendyulianto <graendyulianto@gmail.com>
2026-06-11 11:20:58 +0700

    db kontrak dan customer

--- ntah/db.php
+++ ntah/db.php
@@ -5,8 +5,28 @@ $sql = "CREATE TABLE IF NOT EXISTS barang
 nama_barang VARCHAR(20) NOT NULL,
 jenis VARCHAR(20) NOT NULL,
 stok INT(1) NOT NULL,
-status VARCHAR(20) NULL
+status VARCHAR(20) NULL,
+PRIMARY KEY(id_barang)
 )";
+$hasil = mysqli_query($conn, $sql);
 
+$sql = "CREATE TABLE IF NOT EXISTS customer
+(id_customer VARCHAR(20) NOT NULL,
+nama_lengkap VARCHAR(20) NOT NULL,
+username VARCHAR(20) NULL,
+alamat VARCHAR(20) NOT NULL,
+password VARCHAR(10) NOT NULL,
+no_telp VARCHAR(20) NOT NULL,
+PRIMARY KEY(id_customer)
+)";
+$hasil = mysqli_query($conn, $sql);
+$sql = "CREATE TABLE IF NOT EXISTS kontrak
+(id_kontrak VARCHAR(20) NOT NULL,
+id_order VARCHAR(20) NOT NULL,
+id_customer VARCHAR(20) NOT NULL,
+tgl_proyek DATE NOT NULL,
+tgl_jatuh_tempo DATETIME NOT NULL,
+PRIMARY KEY(id_kontrak)
+)";
 
 ?>
